c8027616b6878e3edc03ff1c65da565b78884950
mingri <3116430062@qq.com>
2024-10-22 23:27:38 +0800

    feat：管理端工程搭建，提供接口

---
+++ dynamic-thread-pool-admin/src/test/java/cn/mingri/middleware/dynamic/thread/pool/test/ApiTest.java
@@ -0,0 +1,11 @@
+package cn.mingri.middleware.dynamic.thread.pool.test;
+
+/**
+ * @Author: mingri31164
+ * @CreateTime: 2024/10/22 21:59
+ * @ClassName: ApiTest
+ * @Version: 1.0
+ */
+public class ApiTest {
+
+}
